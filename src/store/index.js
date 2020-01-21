ef4697fd0466712256a51880f7fe364422145737
Manas Chimpidi <manaschimpidi@gmail.com>
2020-01-21 15:01:10 -0600

    setup

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,15 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+import appData from "../assets/appData.json";
+import drinks from "../assets/drinks.json";
+
 export default new Vuex.Store({
-  state: {},
+  state: {
+    appData,
+    drinks,
+    customerData: {}
+  },
   mutations: {},
   actions: {},
   modules: {}
